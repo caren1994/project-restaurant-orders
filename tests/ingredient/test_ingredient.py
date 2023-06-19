d3fc8c6b20670f322a4adb1fb7122607ce4da511
caren <carenoliveirasilvapontes@gmail.com>
2023-06-19 11:16:42 -0300

    feat: teste ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,30 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (
+    Ingredient,
+    Restriction,
+)  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    bife = Ingredient("bife")
+    queijo = Ingredient("queijo mussarela")
+    queijo2 = Ingredient("queijo mussarela")
+
+    assert bife.name == "bife"
+    assert queijo.name == "queijo mussarela"
+
+    assert bife.__repr__() == "Ingredient('bife')"
+    assert queijo.__repr__() == "Ingredient('queijo mussarela')"
+
+    assert queijo == queijo2
+    assert queijo != bife
+
+    assert hash(bife) == hash("bife")
+    assert hash(queijo) == hash("queijo mussarela")
+    assert hash(bife) != hash(queijo)
+
+    assert queijo.restrictions == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED,
+
+    }
